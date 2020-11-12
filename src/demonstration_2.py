abd473751f7ffb039e30225b4eb1087b17d1490b
David Gold <davidgold@hey.com>
2020-11-12 17:35:39 -0500

    .

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -58,8 +58,15 @@ Constraints:
 - `trust[i][0] != trust[i][1]`
 - `1 <= trust[i][0], trust[i][1] <= N`
 """
-def find_judge(N, trust):
-    """
+"""
+Criteria
+1 - find the ONE node that has no neighbors
+# if there is more than one, return -1
+2 - check if all the other nodes have the node as a neighbor
+# if these aren't satisfied, return -1
+"""
+
+"""
     Inputs:
     N -> int
     trust -> List[List[int]]
@@ -67,5 +74,69 @@ def find_judge(N, trust):
     Output:
     int
     """
-    # Your code here
 
+
+def check_every_other_other_node(graph, candidate):
+    for i in range(1, len(graph)):
+        if i != candidate and candidate not in graph[i]:
+            return False
+    return True
+
+
+print(check_every_other_other_node([[], [3, 4], [3, 4], [], [3]], 3))
+
+
+def find_nodes_with_no_neighbors(graph):
+    candidates = []
+    for i in range(1, len(graph)):
+        if len(graph[i]) == 0:
+            candidates.append(i)
+    ​
+    return candidates
+
+
+​
+print(find_nodes_with_no_neighbors([[], [3, 4], [3, 4], [], [3]]))
+​
+
+
+def build_graph_from_trust(N, trust):
+    graph = [[] for _ in range(N + 1)]
+
+
+​
+   for a, b in trust:
+        graph[a].append(b)
+    ​
+    return graph
+​
+print(build_graph_from_trust(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
+​
+​
+
+
+def find_judge(N, trust):
+    """
+    Inputs:
+    N -> int
+    trust -> List[List[int]]
+​
+    Output:
+    int
+    """
+    # 0. Construct our graph from the `trust` list
+    graph = build_graph_from_trust(N, trust)
+    # 1. Find the one node that has no neighbors
+    candidates = find_nodes_with_no_neighbors(graph)
+    # note: if there is more than one, return -1
+    if len(candidates) != 1:
+        return -1
+    # 2. Check that every other node has this node as a neighbor
+    if check_every_other_other_node(graph, candidates[0]):
+        return candidates[0]
+    # if these both aren't satisfied, return -1
+    return -1
+
+
+​
+print(find_judge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
